c8bd168ca682827d533ef9d300511b24b3982f66
llluferr <rm93205@fiap.com.br>
2022-09-16 11:36:35 -0300

    #

--- tarefas/src/main/java/br/com/fiap/PrimaryController.java
+++ tarefas/src/main/java/br/com/fiap/PrimaryController.java
@@ -9,6 +9,7 @@ import java.util.ResourceBundle;
 import br.com.fiap.model.Cadastro;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
@@ -18,23 +19,40 @@ public class PrimaryController implements Initializable{
     @FXML private TextField textFieldTitulo;
     @FXML private TextField textFieldDescricao; 
     @FXML private TextField textFieldCategoria;
+    @FXML private TextField textFieldConcluida;
     @FXML private DatePicker datePickerData;
+    @FXML private CheckBox checkBoxConcluida;
     @FXML private ListView lvCadastro;
+
 
     private List<Cadastro> lista = new ArrayList<Cadastro>(); 
 
-    public void salvar(){
-        String titulo = textFieldTitulo.getText();
-        String descricao = textFieldDescricao.getText();
-        String categoria = textFieldDescricao.getText();
-        String data = datePickerData.getValue().toString();
+    private void concluidaCadastro() {
 
-        Cadastro cadastro = new Cadastro(titulo, descricao, categoria, data);
-        lista.add(cadastro);
-        lvCadastro.getItems().addAll(lista);
+            String titulo = textFieldTitulo.getText();
+            String descricao = textFieldDescricao.getText();
+            String categoria = textFieldCategoria.getText();
+            String data = datePickerData.getValue().toString();
+             
+            Cadastro cadastro = new Cadastro(titulo, descricao, categoria, data);
+            lista.add(cadastro);
+        
+    }    
+
+    private void naoConcluidaCadastro(){
+            String titulo = textFieldTitulo.getText();
+            String descricao = textFieldDescricao.getText();
+            String categoria = textFieldCategoria.getText();
+            String data = datePickerData.getValue().toString();
+             
+            Cadastro cadastro = new Cadastro(titulo, descricao, categoria, data);
+            lista.add(cadastro);
     }
+
+    
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
     }
 }
+    
